ffe1d48e6b99a580e088b43abea24a1d261f50c9
rana135 <rana286090@gmail.com>
2023-04-15 01:28:25 +0600

    repo opening and updating rules

--- pycharm-github-create-repo.py
+++ pycharm-github-create-repo.py
@@ -1,13 +1,13 @@
 # Pycharm Github Create/Commit/Push for one time every Repository:-
 '''
 VCS --> Create Git Repository --> (Create Repository in Github)
-VSC --> Import into version control/Github --> Share project on Github --> Repository Name (Repository Create Inco)
-VCS --> Commit --> Commit Message  (Doing update Commit)
-VCS --> Git --> Push (Push after Changes)
+VSC --> Import into version control/Github --> Share project on Github --> Repository Name (Repository Create Info)
+VCS --> Commit --> Commit Message  (initialize commit)
+VCS --> Git --> Push (initialize Push)
 '''
 
 # Update any info then commit and push everytime:-
 '''
-VCS --> Commit --> Commit Message  (Doing update Commit)
-VCS --> Git --> Push (Push after Changes)
+VCS --> Commit --> Commit Message  (Commit according change)
+VCS --> Git --> Push (Push if Changes)
 '''
