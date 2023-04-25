06031911551073e34c35878f0c4ccbdabf14988e
rana135 <rana286090@gmail.com>
2023-04-25 15:25:23 +0600

    assignment, comparison, logical Operator example

---
+++ Day-14-assignment-operator.py
@@ -0,0 +1,44 @@
+'''
+Arithmetic কাজগুলোকে কোন একটা variable এর মধ্যে assign করে রাখার মাধ্যমে mathmatical কোন কাজ করি
+তখন তাকে assignment operator বলে।
+'''
+x = 10
+x += 3    # Same As : x = x + 3
+print(x)  # 13
+
+y = 10
+y -= 3    # Same As : y = y - 3
+print(y)  # 7
+
+x = 5
+x *= 3    # Same As : x = x * 3
+print(x)  # 15
+
+x = 5
+x /= 3    # Same As : x = x / 3
+print(x)  # 1.6666666666666667
+
+x = 5
+x %= 3    # Same As : x = x % 3
+print(x)  # 2
+
+x = 5
+x **= 3   # Same As : x = x ** 3
+print(x)  # 125
+
+
+# Comparison Operator:
+x = 5
+y = 3
+print(x == y)   # False
+print(x != y)   # True
+print(x > y)    # True
+print(x < y)    # False
+print(x >= y)   # True
+print(x <= y)   # False
+
+
+# Python Logical Operators
+print(x > 3 and x < 10)         # True
+print(x < 5 or x < 4)           # False
+print(not(x < 5 and x < 10))    # True
