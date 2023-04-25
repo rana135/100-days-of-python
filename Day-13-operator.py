4f96cef88c5dd7397a5d84ed5feb7b8dfd840a45
rana135 <rana286090@gmail.com>
2023-04-25 12:28:22 +0600

    Operator example

---
+++ Day-13-operator.py
@@ -0,0 +1,20 @@
+'''
+There are different types of JavaScript operators:
+Arithmetic Operators
+Assignment Operators
+Comparison Operators
+String Operators
+Logical Operators
+Bitwise Operators
+Ternary Operators
+Type Operators
+'''
+
+x = 5
+y = 2
+print(x + y)   # 7
+print(x - y)   # 3
+print(x * y)   # 10
+print(x / y)   # 2.5
+print(x ** y)  # 25  [y এর value যত থাকবে ততবার x কে multiple করবে]
+print(x // y)  # 2   [division এর মতোই কাজ just difference হলো এটি divide করে output কে floor করে ফেলবে।]
