d384cd5485f36b64d8af5e370f6f741543bdc12f
rana135 <rana286090@gmail.com>
2023-04-25 20:39:52 +0600

    take input from user use input method

---
+++ Day-16-user-input.py
@@ -0,0 +1,10 @@
+user = input("Enter Your User-Name: ")
+password = input("Enter Your pass: ")
+print(user)
+print(password)
+'''
+Enter Your User-Name: Rana
+Enter Your pass: 123456
+Rana
+123456
+'''
