6dc67bae7dee37f8da61e02e818ca09d61ce6560
rana135 <rana286090@gmail.com>
2023-04-25 11:02:09 +0600

    None type data example

---
+++ Day-10-none-type-data.py
@@ -0,0 +1,16 @@
+'''None is not defined to be 0, False, Empty string or any other value. In Python, None is an object.'''
+x=None
+print(x)         # None
+print(type(x))   # NoneType
+
+# None just None কারন False,string,0 এদের নিজস্ব data type রয়েছে। Other language এ None কে null বলে ।
+A=False
+print(type(A))   # bool
+
+y=''
+print(type(y))   # str
+
+z=0
+print(type(z))   # int
+
+
