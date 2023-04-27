dac27755a7787115131a8d45383ccd57f93a3814
rana135 <rana286090@gmail.com>
2023-04-27 13:39:56 +0600

    matrix access

---
+++ Day-29-matrix.py
@@ -0,0 +1,10 @@
+# A matrix in Python is a two-dimensional array having a specific number of rows and columns.
+# যাকে প্রথমে row বরাবর কে ধরা হয় ও পরে col অনুসারে ধরা হয়।
+matrixlist = [
+    [1,2,3,4,5],
+    [6,7,8,9,10]
+]
+print(matrixlist[1][3])     # 9
+print(matrixlist[0][4])     # 5
+print(matrixlist[1][0])     # 6
+
